28d96f865aeda38ff2b1e5777eb1c6781208d6e4
Arfan Uddin <arfan.uddin@voiceriver.com>
2021-03-21 16:55:41 -0700

    update UI changes

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reacts
+            Fib Calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
